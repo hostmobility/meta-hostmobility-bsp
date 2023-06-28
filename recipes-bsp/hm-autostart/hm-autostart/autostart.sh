972fd6426961f1562865d47adeab768982fe4a1e
rikardo <rikard.olander@hostmobility.com>
2023-06-28 14:20:55 +0000

    HMX:autostart.sh: Add hostname with serial number

--- recipes-bsp/hm-autostart/hm-autostart/autostart.sh
+++ recipes-bsp/hm-autostart/hm-autostart/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+MACHINE=$(cat /etc/platform-system-type)
 #set RGB LED Green
-if [[ "$(cat /etc/platform-system-type)" == "imx8mp-var-dart-hmx1" ]]; then
+if [[ "$MACHINE" == "imx8mp-var-dart-hmx1" ]]; then
 	RGB_I2C_ID=$(fw_printenv RGB_I2C_ID | sed 's/RGB_I2C_ID=//')
 	if [[ -z "$RGB_I2C_ID" ]]; then
 		i2cset -y 1 0x32 8 0x50;
@@ -14,10 +14,26 @@ if [[ "$(cat /etc/platform-system-type)" == "imx8mp-var-dart-hmx1" ]]; then
 	fi
 fi
 
+
 FIRST_TIME_BOOT_FILE=/etc/first_boot_after_update.txt
 
 if [[ -f $FIRST_TIME_BOOT_FILE ]];
 then
+
+#Set hostname to ${MACHINE}-SERIAL. TODO this could be compatible with MXV(&& "$MACHINE" == "mx5-pt").
+if [[ "$MACHINE" == "imx8mp-var-dart-hmx1" ]]; then
+	# Retrieve the serial number from /proc/device-tree/chosen/serial-number
+	serial_number=0
+	serial_number=$(cat /proc/device-tree/chosen/serial-number)
+	if [[ "$MACHINE" == "imx8mp-var-dart-hmx1" ]]; then
+		hostname="HMX-${serial_number}"
+	else
+		hostname="${MACHINE}-${serial_number}"
+	fi
+	# Update the hostname
+	echo "$hostname" > /etc/hostname
+	hostname "$hostname"
+fi
 
 # Remove /boot/*.scr from boot so next reboot will not start a new upgrade.
 rm /boot/*.scr

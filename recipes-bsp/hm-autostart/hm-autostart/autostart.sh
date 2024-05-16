e6bd5f8695710599456d0a7844ed73baefc10550
rikardo <rikard.olander@hostmobility.com>
2024-05-16 12:28:42 +0000

    hmx: autostart: add set start state for host_watchdog
    This will make te cocpu go from boot state to
    start/running state and in this mode the driver
    will send hearbeats and the co_cpu respones on
    heartbeat if the link breaks it will result in
    a reset of either side.

--- recipes-bsp/hm-autostart/hm-autostart/autostart.sh
+++ recipes-bsp/hm-autostart/hm-autostart/autostart.sh
@@ -5,6 +5,11 @@ MACHINE=$(cat /etc/platform-system-type)
 
 FIRST_TIME_BOOT_FILE=/etc/first_boot_after_update.txt
 
+# We can now change the state of the host watchdog from boot to start. This will enable heartbeats resetting the watchdog timer, preventing a reset of the system.
+if [[ "$MACHINE" == "imx8mp-var-dart-hmx1" ]]; then
+	echo "0" > /sys/class/host_watchdog/ctl_start
+fi
+
 if [[ -f $FIRST_TIME_BOOT_FILE ]];
 then
 

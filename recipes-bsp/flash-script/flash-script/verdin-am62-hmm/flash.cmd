d65f8c27fb74ba8432090c65498b650b7ff694fd
rikardo <rikard.olander@hostmobility.com>
2025-02-04 10:38:20 +0000

    hmm:usb_boot_script: fix leds and remove reset to prevent flashing forever loop

--- recipes-bsp/flash-script/flash-script/verdin-am62-hmm/flash.cmd
+++ recipes-bsp/flash-script/flash-script/verdin-am62-hmm/flash.cmd
@@ -6,13 +6,26 @@ env set wic_file hmm-image.wic.gz
 
 # Start USB device and try to load wic_file into memory if load fails, exit the script (and boot normally).
 
+#Set LEDs 1 to 4 off.
+led green:led_1 off
+led green:led_2 off
+led green:led_3 off
+led green:led_4 off
+
+led red:led_1 off
+led red:led_2 off
+led red:led_3 off
+led red:led_4 off
+
 env set loadimage_usb "load usb ${usbdev} ${ramdisk_addr_r} ${wic_file}"
 echo Loading ${wic_file} from USB ${usbdev};
 if run loadimage_usb; then
     echo load ok;
-else 
+    led green:led_3 on
+else
     echo load failed, aborting;
     sleep 10;
+    led red:led_3 on
     reset;
 fi
 
@@ -21,10 +34,11 @@ env set writeimage "gzwrite mmc 0 ${ramdisk_addr_r} 0x${filesize} 100000 0"
 echo Extracting ${wic_file} to mmc 0;
 
 if run writeimage; then
-    echo write ok;
-    reset;
+    echo write ok, continue booting up the new system (no new uboot env!);
+    led green:led_4 on
 else
     echo write failed, aborting;
     sleep 10;
+    led red:led_4 on
     reset;
 fi
